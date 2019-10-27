3b776d6739c7d706872198f42658f2d9bb6827e5
comeonbabyzy <zhangye98@gmail.com>
2019-10-27 18:51:06 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 PROJECT_HOME="~/content/traffic_police"
+RESEARCH_HOME="~/content/models/research"
 TRAIN_IMAGE=$PROJECT_HOME"/train/images"
 EVAL_IMAGE=$PROJECT_HOME"/validation/images"
 TRAIN_XML=$PROJECT_HOME"/train/annotations"
 EVAL_XML=$PROJECT_HOME"/validation/annotations"
 
+
 PRETRAINED_MODEL="ssd_mobilenet_v1_coco_2018_01_28"
 PRETRAINED_MODEL_FILE=$PRETRAINED_MODEL".tar.gz"
 PRETRAINED_MODEL_URL="http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2018_01_28.tar.gz"
 PIPELINE_CONFIG_URL="https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/samples/configs/ssd_mobilenet_v1_coco.config"
-PIPELINE_CONFIG_PATH=$PROJECT_HOME"\ssd_mobilenet_v1_coco.config"
+PIPELINE_CONFIG_PATH=$PROJECT_HOME"/ssd_mobilenet_v1_coco.config"
 
 TRAIN_INPUT_PATH=$PROJECT_HOME"/train.record"
 EVAL_INPUT_PATH=$PROJECT_HOME"/validation.record"
@@ -29,15 +31,10 @@ echo $LABEL_MAP_PATH
 echo $FINE_TUNE_CHECKPOINT
 echo $NUM_EXAMPLES
 
-
 #cd $PROJECT_HOME
 
-cd ~/
 rm -rf $PROJECT_HOME
-unzip traffic_police.zip
-cp -r traffic_police ~/content
-
-exit
+unzip ~/traffic_police.zip -d ~/content
 
 python3 xml_to_csv.py --xml_path=$TRAIN_XML --csv_output=$PROJECT_HOME"/train.csv"
 python3 xml_to_csv.py --xml_path=$EVAL_XML --csv_output=$PROJECT_HOME"/validation.csv"
@@ -49,14 +46,10 @@ cp "traffic_police.pbtxt" $PROJECT_HOME
 
 cd $PROJECT_HOME
 
-
-
 wget $PRETRAINED_MODEL_URL
 tar zxvf $PRETRAINED_MODEL_FILE
 
 wget $PIPELINE_CONFIG_URL
-
-
 
 #sed -i 's/input_path: "PATH_TO_BE_CONFIGURED\/mscoco_train.record.*"/input_path: "\/content\/train.record"/g' $PIPELINE_CONFIG_PATH
 #sed -i 's/input_path: "PATH_TO_BE_CONFIGURED\/mscoco_val.record.*"/input_path: "\/content\/validation.record"/g' $PIPELINE_CONFIG_PATH
@@ -72,12 +65,12 @@ sed -i 's/num_examples.*/num_examples: '$NUM_EXAMPLES'/g' $PIPELINE_CONFIG_PATH
 
 sed -i 's/fine_tune_checkpoint:.*/fine_tune_checkpoint: "'$FINE_TUNE_CHECKPOINT'"/g' $PIPELINE_CONFIG_PATH
 
+exit
 
 # From the tensorflow/models/research/ directory
-cd /content/models/research
+cd $RESEARCH_HOME
 #PIPELINE_CONFIG_PATH=/content/faster_rcnn_inception_v2_pets.config
-PIPELINE_CONFIG_PATH=/content/ssd_mobilenet_v1_coco.config
-MODEL_DIR=/content
+MODEL_DIR=$PROJECT_HOME
 NUM_TRAIN_STEPS=200000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python object_detection/model_main.py \
