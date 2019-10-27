9419a225a68625ad5eeb64ed240e77e472820da5
comeonbabyzy <zhangye98@gmail.com>
2019-10-27 16:46:42 +0800

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -13,6 +13,8 @@ sudo pip3 install matplotlib
 sudo pip3 install tensorboardcolab
 sudo pip3 install jupyter
 
+cd ~/
+
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.10.0/protoc-3.10.0-linux-x86_64.zip
 unzip protoc-3.10.0-linux-x86_64.zip 
 cp bin/protoc /usr/local/bin
@@ -25,5 +27,7 @@ export PYTHONPATH=$PYTHONPATH";`pwd`;`pwd`/slim"
 
 echo `pwd` > /usr/lib/python3.6/site-packages/tensorflow_model.pth
 echo `pwd\slim' >> /usr/lib/python3.6/site-packages/tensorflow_model.pth
+
+protoc object_detection/protos/*.proto --python_out=.
 
 python3 object_detection/builders/model_builder_test.py
