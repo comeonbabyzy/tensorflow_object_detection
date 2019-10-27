0bbf1d926c698690d227c78ea539e23f656275a9
comeonbabyzy <zhangye98@gmail.com>
2019-10-27 19:39:02 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -72,7 +72,7 @@ cd $RESEARCH_HOME
 MODEL_DIR=$PROJECT_HOME
 NUM_TRAIN_STEPS=200000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python object_detection/model_main.py \
+python3 object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
