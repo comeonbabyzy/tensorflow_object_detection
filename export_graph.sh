308a2890d93afd43a1395bab6956cc340257434c
comeonbabyzy <zhangye98@gmail.com>
2019-10-29 11:18:57 +0800

    Update export_graph.sh

--- export_graph.sh
+++ export_graph.sh
@@ -2,8 +2,17 @@
 
 cd $RESEARCH_HOME
 
-python object_detection/export_inference_graph.py \
-  --input_type=image_tensor \
-  --pipeline_config_path=$PIPELINE_CONFIG_PATH \
-  --trained_checkpoint_prefix=$PROJECT_HOME"/model.ckpt-184602" \
-  --output_directory=$PROJECT_HOME
+CHECKPOINT_PATH=$PROJECT_HOME"/model.ckpt-184602" 
+
+
+#python object_detection/export_inference_graph.py \
+#  --input_type=image_tensor \
+#  --pipeline_config_path=$PIPELINE_CONFIG_PATH \
+#  --trained_checkpoint_prefix=$CHECKPOINT_PATH \
+#  --output_directory=$PROJECT_HOME
+
+python object_detection/export_tflite_ssd_graph.py \
+--pipeline_config_path=$PIPELINE_CONFIG_PATH \
+--trained_checkpoint_prefix=$CHECKPOINT_PATH \
+--output_directory=$PROJECT_HOME"/tflite \
+--add_postprocessing_op=true
