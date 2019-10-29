2a87672f6a4095f4dd39a1023b743a6ffba48512
comeonbabyzy <zhangye98@gmail.com>
2019-10-29 13:43:42 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -2,15 +2,16 @@
 
 PROJECT_HOME="/home/opc/content/traffic_police"
 RESEARCH_HOME="/home/opc/content/models/research"
+PIPELINE_CONFIG_PATH=$PROJECT_HOME"/ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync.config"
 
 export PYTHONPATH=$PYTHONPATH":"$RESEARCH_HOME":"$RESEARCH_HOME"/slim"
 
 # From the tensorflow/models/research/ directory
 cd $RESEARCH_HOME
 MODEL_DIR=$PROJECT_HOME
-PIPELINE_CONFIG_PATH=$PROJECT_HOME"/ssd_mobilenet_v1_coco.config"
 NUM_TRAIN_STEPS=200000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
+
 python object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
