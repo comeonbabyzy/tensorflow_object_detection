8f8869bb979e72b0be69f491e036365534076014
comeonbabyzy <zhangye98@gmail.com>
2019-10-29 11:05:46 +0800

    Update export_graph.sh

--- export_graph.sh
+++ export_graph.sh
@@ -2,8 +2,8 @@
 
 cd $RESEARCH_HOME
 
-python export_inference_graph.py \
+python object_detection/export_inference_graph.py \
   --input_type=image_tensor \
-  --pipeline_config_path=$PIPELINE_CONFIG_PATH
-  --trained_checkpoint_prefix=$PROJECT_HOME"/model.ckpt-184602"
+  --pipeline_config_path=$PIPELINE_CONFIG_PATH \
+  --trained_checkpoint_prefix=$PROJECT_HOME"/model.ckpt-184602" \
   --output_directory=$PROJECT_HOME
