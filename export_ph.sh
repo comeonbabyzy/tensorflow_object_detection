a21a8aacd4c4da5e7c567d9d53202ab348074f43
comeonbabyzy <zhangye98@gmail.com>
2019-11-01 14:57:23 +0800

    Update export_ph.sh

--- export_ph.sh
+++ export_ph.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=$PYTHONPATH":"$RESEARCH_HOME":"$RESEARCH_HOME"/slim"
 PIPELINE_CONFIG=$PROJECT_HOME"/pipeline.config"
 
 cd $RESEARCH_HOME
-python export_inference_graph.py \
+python object_detection/export_inference_graph.py \
   --input_type=image_tensor \
   --pipeline_config_path=$PIPELINE_CONFIG \
   --trained_checkpoint_prefix=$PROJECT_HOME"/model.ckpt-190120" \
